d417ef9ff098da071d400064219f4dcfaa2f862a
Alice Bagoud <99303912+alicebagoud@users.noreply.github.com>
2022-12-13 17:17:19 -0500

    main

--- assignment-JS-2/calculator1/main.js
+++ assignment-JS-2/calculator1/main.js
@@ -1,4 +1,4 @@
-class Calculator {
+/* class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement
     this.currentOperandTextElement = currentOperandTextElement
@@ -163,3 +163,4 @@ document.addEventListener('keydown', function (event) {
   }
 
 });
+ */
