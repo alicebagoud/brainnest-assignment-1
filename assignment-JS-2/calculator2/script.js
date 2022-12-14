b82d3dde2e397394361496595a9468f45ce536b1
Alice Bagoud <99303912+alicebagoud@users.noreply.github.com>
2022-12-13 19:24:26 -0500

    main

--- assignment-JS-2/calculator2/script.js
+++ assignment-JS-2/calculator2/script.js
@@ -1,12 +1,86 @@
-/* const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
- */
+let display = document.getElementById("display");
 
-const output = document.getElementsByClassName("output");
+let buttons = Array.from(document.getElementsByClassName("button"));
 
-const buttons = Array.from(document.getElementsByClassName)
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'AC':
+                display.innerText = '';
+                break;
+            case 'DEL':
+                if(display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1); /* splice method to remove the last character from our display */
+                }
+                break;
+            case '=':
+                try {
+                    display.innerText = Math.round(eval(display.innerText));
+                } catch {
+                    display.innerText = '*****';
+                }
+                break;            
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
+
+function getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
+  }
+
+
+// keyboard Support
+
+
+document.addEventListener('keydown', function (event) {
+    let patternForNumbers = /[0-9]/g;
+    let patternForOperators = /[+-/*]/g;
+  
+    if (event.key.match(patternForNumbers)) {
+      event.preventDefault();
+      buttons.appendNumber(event.key)
+      buttons.updateDisplay()
+    }
+    if (event.key === '.') {
+      event.preventDefault();
+      buttons.appendNumber(event.key)
+      buttons.updateDisplay()
+    }
+    if (event.key.match(patternForOperators)) {
+      event.preventDefault();
+      buttons.chooseOperation(event.key)
+      buttons.updateDisplay()
+    }
+    if (event.key === 'Enter' || event.key === '=') {
+      event.preventDefault();
+      buttons.compute()
+      buttons.updateDisplay()
+    }
+    if (event.key === "Backspace") {
+      event.preventDefault();
+      buttons.delete()
+      buttons.updateDisplay()
+    }
+    if (event.key == 'Delete') {
+      event.preventDefault();
+      buttons.clear()
+      buttons.updateDisplay()
+    }
+  
+  });
+  
